cbbb7a0079e322e107c79f13c9b13aab97c035e3
Ilia <istudyatuni@gmail.com>
2021-02-26 17:03:03 +0300

    Лабораторное занятие 18.02.2021, визуализация и мультимедиа

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Run build (F7) from file with report, folder with this file must contain file attachment.md with source code in one file (it compile after other content from report)
 
 `attachment.md` compiled with file-merger and use config from file-merger.yml
+
+All images to insert in pdf file must contains in `folder_with_report/imgs`
--- build.sh
+++ build.sh
@@ -10,11 +10,13 @@ ext=$1
 fname=$2
 res=$3
 meta=$4
-conf_folder=$5
+file_folder=$5
 
 if [ "$ext" == "md" ]; then
-	python ~/scripts/file-merger/file_merger.py -c "$conf_folder/file-merger.yml"
-	pandoc $fname -o $res --pdf-engine=xelatex --metadata-file=$meta --tab-stop=2
+	if [[ -f "$file_folder/file-merger.yml" ]]; then
+		python ~/scripts/file-merger/file_merger.py -c "$file_folder/file-merger.yml"
+	fi
+	pandoc $fname -o $res --pdf-engine=xelatex --metadata-file=$meta --tab-stop=2 --resource-path=$file_folder/imgs
 else
 	echo $usage
 	echo 'Wrong file type'
